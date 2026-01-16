04972e8b1a8119c407a8bfc9ff498b1fc5198cd4
Allmuted <65719689+a11mut3d@users.noreply.github.com>
2026-01-17 02:34:49 +0500

    Update main.py

--- src/fullmute/main.py
+++ src/fullmute/main.py
@@ -2,11 +2,25 @@ import sys
 import click
 from fullmute.cli.commands import cli
 
+def print_ascii_art():
+    ascii_art = r"""
+ _____ _     _     _     _      _    _____ _____
+/    // \ /\/ \   / \   / \__/|/ \ /Y__ __Y  __/
+|  __\| | ||| |   | |   | |\/||| | || / \ |  \  
+| |   | \_/|| |_/\| |_/\| |  ||| \_/| | | |  /_ 
+\_/   \____/\____/\____/\_/  \|\____/ \_/ \____\
+    """
+    print(ascii_art)
+    print("FullMute - Advanced Web Scanner\n")
+
 def entrypoint():
     if len(sys.argv) == 1:
+        print_ascii_art()
         sys.argv.append('--help')
 
     try:
+        if len(sys.argv) > 1 and sys.argv[1] not in ['--help', '-h', 'help']:
+            print_ascii_art()
         cli()
     except KeyboardInterrupt:
         print("\n\nScan interrupted by user")
