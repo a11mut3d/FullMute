b0c2ddf1c41353c583a5be55f4605307c86ac13e
Allmuted <65719689+a11mut3d@users.noreply.github.com>
2026-04-26 16:47:41 -0400

    Create __init__.py

---
+++ src/fullmute/scanner/__init__.py
@@ -0,0 +1,17 @@
+from fullmute.scanner.port_scanner import (
+    PortScanner,
+    PortScanResult,
+    scan_ports,
+    scan_ports_with_cves,
+    TOP_20_PORTS,
+    PORT_SERVICE_MAP
+)
+
+__all__ = [
+    'PortScanner',
+    'PortScanResult',
+    'scan_ports',
+    'scan_ports_with_cves',
+    'TOP_20_PORTS',
+    'PORT_SERVICE_MAP',
+]
