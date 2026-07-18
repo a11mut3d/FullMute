009e5f131ef77802047abf52a2281378115d2653
Allmuted <65719689+a11mut3d@users.noreply.github.com>
2026-07-18 13:16:06 -0400

    Update plugin_detector.py

--- src/fullmute/detector/plugin_detector.py
+++ src/fullmute/detector/plugin_detector.py
@@ -13,20 +13,20 @@ class PluginDetector:
     def detect_plugins(self) -> dict:
         results = {}
 
-        
+
         wp_plugins = self.detect_wordpress_plugins()
         if wp_plugins:
             results['wordpress'] = wp_plugins
 
-        
+
         wp_themes = self.detect_wordpress_themes()
         if wp_themes:
             if 'wordpress' not in results:
                 results['wordpress'] = []
-            
+
             results['wordpress_themes'] = wp_themes
 
-        
+
         joomla_exts = self.detect_joomla_extensions()
         if joomla_exts:
             results['joomla'] = joomla_exts
@@ -42,46 +42,37 @@ class PluginDetector:
         return results
 
     def detect_wordpress_plugins(self) -> List[Tuple[str, str]]:
-        plugins = set()  
+        plugins = set()
 
-        
+
         plugin_pattern1 = r'/wp-content/plugins/([^/\"\'>]+)/'
         matches1 = re.findall(plugin_pattern1, self.html, re.IGNORECASE)
         for plugin in matches1:
-            
+
             version = self._extract_version_from_path(f'/wp-content/plugins/{plugin}/')
             plugins.add((plugin, version))
 
-        
+
         plugin_pattern2 = r'wp-content[\/\\]plugins[\/\\]([^\/\\\"\'>\s]+)/'
         matches2 = re.findall(plugin_pattern2, self.html, re.IGNORECASE)
         for plugin in matches2:
             version = self._extract_version_from_path(f'wp-content/plugins/{plugin}/')
             plugins.add((plugin, version))
 
-        
+
         resource_pattern = r'(?:href|src)=["\'][^"\']*wp-content[\/\\]plugins[\/\\]([^\/\\\"\'>\s]+)[\/\\][^"\']*["\']'
         matches3 = re.findall(resource_pattern, self.html, re.IGNORECASE)
         for plugin in matches3:
             version = self._extract_version_from_path(f'wp-content/plugins/{plugin}/')
             plugins.add((plugin, version))
 
-        
+
         json_pattern = r'["\']wp[-_]?(?:plugin|extension)["\']\s*:\s*["\']([^"\']+)["\']'
         matches4 = re.findall(json_pattern, self.html, re.IGNORECASE)
         for plugin in matches4:
-            
+
             clean_plugin = re.sub(r'[^\w\-]', '', plugin)
             if len(clean_plugin) > 2:
-                version = self._extract_version_from_text(clean_plugin, self.html)
-                plugins.add((clean_plugin, version))
-
-        
-        comment_pattern = r'<!--.*?(?:powered by|developed by|by|plugin).*?([^,\s\'\"<>]+).*?-->'
-        matches5 = re.findall(comment_pattern, self.html, re.IGNORECASE)
-        for plugin in matches5:
-            clean_plugin = re.sub(r'[^\w\-]', '', plugin)
-            if len(clean_plugin) > 2 and self._is_likely_plugin(clean_plugin):
                 version = self._extract_version_from_text(clean_plugin, self.html)
                 plugins.add((clean_plugin, version))
 
@@ -90,30 +81,30 @@ class PluginDetector:
     def detect_wordpress_themes(self) -> List[Tuple[str, str]]:
         themes = set()
 
-        
+
         theme_pattern1 = r'/wp-content/themes/([^/\"\'>]+)/'
         matches1 = re.findall(theme_pattern1, self.html, re.IGNORECASE)
         for theme in matches1:
             version = self._extract_version_from_path(f'/wp-content/themes/{theme}/')
             themes.add((theme, version))
 
-        
+
         theme_pattern2 = r'wp-content[\/\\]themes[\/\\]([^\/\\\"\'>\s]+)/'
         matches2 = re.findall(theme_pattern2, self.html, re.IGNORECASE)
         for theme in matches2:
             version = self._extract_version_from_path(f'wp-content/themes/{theme}/')
             themes.add((theme, version))
 
-        
+
         resource_pattern = r'(?:href|src)=["\'][^"\']*wp-content[\/\\]themes[\/\\]([^\/\\\"\'>\s]+)[\/\\][^"\']*["\']'
         matches3 = re.findall(resource_pattern, self.html, re.IGNORECASE)
         for theme in matches3:
             version = self._extract_version_from_path(f'wp-content/themes/{theme}/')
             themes.add((theme, version))
 
-        
+
         meta_pattern = r'(?:name|class)=["\'][^"\']*["\'][^>]*>\s*[^<]*["\']([^"\'>\s]+)["\']'
-        
+
         generator_pattern = r'content=["\'][^"\']*by[^"\']*([^"\'>\s]+)[^"\']*["\'][^>]*name=["\'][^"\']*generator'
         matches4 = re.findall(generator_pattern, self.html, re.IGNORECASE)
         for theme in matches4:
@@ -127,7 +118,7 @@ class PluginDetector:
     def detect_joomla_extensions(self) -> List[Tuple[str, str]]:
         extensions = set()
 
-        
+
         comp_pattern = r'/components/com_([^/\"\'>]+)/'
         matches1 = re.findall(comp_pattern, self.html, re.IGNORECASE)
         for ext in matches1:
@@ -151,16 +142,16 @@ class PluginDetector:
     def detect_drupal_modules(self) -> List[Tuple[str, str]]:
         modules = set()
 
-        
+
         mod_pattern = r'/modules/([^/\"\'>\s]+)/'
         matches = re.findall(mod_pattern, self.html, re.IGNORECASE)
         for mod in matches:
-            
+
             if mod not in ['system', 'user', 'node', 'views']:
                 version = self._extract_version_from_path(f'/modules/{mod}/')
                 modules.add((mod, version))
 
-        
+
         contrib_pattern = r'/sites/(?:all/)?modules/contrib/([^/\"\'>\s]+)/'
         contrib_matches = re.findall(contrib_pattern, self.html, re.IGNORECASE)
         for mod in contrib_matches:
@@ -170,8 +161,8 @@ class PluginDetector:
         return list(modules)
 
     def _extract_version_from_path(self, path_fragment: str) -> str:
-        
-        
+
+
         escaped_fragment = re.escape(path_fragment.replace('/', '/'))
         version_patterns = [
             rf'{escaped_fragment}[^"\']*?([0-9]+\.[0-9]+(?:\.[0-9]+)?)',
@@ -187,7 +178,7 @@ class PluginDetector:
         return ""
 
     def _extract_version_from_text(self, item_name: str, text: str) -> str:
-        
+
         patterns = [
             rf'{re.escape(item_name)}[^a-zA-Z0-9]*v?([0-9]+\.[0-9]+(?:\.[0-9]+)?)',
             rf'{re.escape(item_name)}[^a-zA-Z0-9]*([0-9]+\.[0-9]+(?:\.[0-9]+)?)',
@@ -197,7 +188,7 @@ class PluginDetector:
         for pattern in patterns:
             match = re.search(pattern, text, re.IGNORECASE)
             if match and len(match.groups()) > 0:
-                
+
                 version_group = match.groups()[-1]
                 if re.match(r'^[0-9]+(\.[0-9]+)*$', version_group):
                     return version_group
@@ -217,7 +208,7 @@ class PluginDetector:
     def detect_bitrix_components(self) -> List[Tuple[str, str]]:
         components = set()
 
-        
+
         comp_pattern = r'/bitrix/components/([^/\"\'\s]+)/([^/\"\'\s]+)/'
         matches = re.findall(comp_pattern, self.html, re.IGNORECASE)
         for vendor, comp_name in matches:
@@ -225,20 +216,20 @@ class PluginDetector:
             version = self._extract_version_from_path(f'/bitrix/components/{vendor}/{comp_name}/')
             components.add((full_comp, version))
 
-        
+
         bitrix_comp_pattern = r'bitrix:([^\'\"\s>]+)'
         matches = re.findall(bitrix_comp_pattern, self.html, re.IGNORECASE)
         for comp in matches:
             version = self._extract_version_from_text(comp, self.html)
             components.add((comp, version))
 
-        
+
         template_pattern = r'/bitrix/templates/([^/\"\'\s]+)/'
         matches = re.findall(template_pattern, self.html, re.IGNORECASE)
         for template in matches:
             components.add((f"template:{template}", ""))
 
-        
+
         include_pattern = r'IncludeComponent\(\s*"([^"]+)"'
         matches = re.findall(include_pattern, self.html, re.IGNORECASE)
         for comp in matches:
